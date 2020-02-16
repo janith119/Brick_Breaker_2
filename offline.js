4bd6c1a9092627b2fe1b26118b69d1bb3d89b983
Janith <shrinkedup@gmail.com>
2020-02-16 13:05:06 +0530

    second commit

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1581783691,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/blue-sheet0.png",
+		"images/green-sheet0.png",
+		"images/yellow-sheet0.png",
+		"images/brown-sheet0.png",
+		"images/pink-sheet0.png",
+		"images/red-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/paddle-sheet0.png",
+		"images/wall-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
